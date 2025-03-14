425f010ed55489f51a6440c74d7b0f1c4bae5a54
Kevin Dobruskin <bailydobruskin@gmail.com>
2025-03-14 11:11:01 -0400

    deleted dashed out code

--- Main/GameFolder/MultiplayerHandler.java
+++ Main/GameFolder/MultiplayerHandler.java
@@ -20,7 +20,7 @@ public class MultiplayerHandler implements Runnable {
 
     }
 
-   @Override
+    @Override
     public void run() {
         System.out.println("MultiplayerHandler is running...");
         while (true) {
@@ -28,39 +28,16 @@ public class MultiplayerHandler implements Runnable {
                 if (isServer) {
                     out.writeObject(multiplayerGameInfo);
                     out.reset();
-                    multiplayerGameInfo.updateInfoForPlayer1(((MultiplayerGameInfo)in.readObject()).paddle2Y);
+                    multiplayerGameInfo.updateInfoForPlayer1(((MultiplayerGameInfo) in.readObject()).paddle2Y);
                 } else {
-                  multiplayerGameInfo.updateInfoForPlayer2((MultiplayerGameInfo) in.readObject());
-                   out.writeObject(multiplayerGameInfo);
+                    multiplayerGameInfo.updateInfoForPlayer2((MultiplayerGameInfo) in.readObject());
+                    out.writeObject(multiplayerGameInfo);
                     out.reset();
-
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
-   
-    // @Override
-    // public void run() {
-    //     while (true) {
-    //         try {
-    //             if (isServer) {
-    //                 out.writeObject(multiplayerGameInfo);
-    //                 out.reset();
-    //                 game.paddle2.multiplayerMove(in.readInt());
-    //             } else {
-    //                 multiplayerGameInfo = (MultiplayerGameInfo) in.readObject();
-    //                 out.writeInt(multiplayerGameInfo.paddle2Y);
-    //                 out.reset();
-    //                 game.paddle1.multiplayerMove(multiplayerGameInfo.paddle1Y);
-    //                 game.ball.multiplayerMove(multiplayerGameInfo.ballX, multiplayerGameInfo.ballY);
-
-    //             }
-    //         } catch (Exception e) {
-    //             e.printStackTrace();
-    //         }
-    //     }
-    // }
 
 }
